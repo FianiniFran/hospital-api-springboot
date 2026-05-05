3ea9a829e6828d5065e3ed5d4fe3589f1754e809
Franco <francofianinisc@gmail.com>
2026-05-05 14:43:12 -0300

    fix: changed primitive types to their wrapper classes

--- src/main/java/apiRest/hospital/entities/Doctor.java
+++ src/main/java/apiRest/hospital/entities/Doctor.java
@@ -20,13 +20,13 @@ import lombok.NoArgsConstructor;
 public class Doctor {
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
-	private int id;
+	private Integer id;
 	@Column(name="first_name")
 	private String name;
 	@Column(name="last_name")
 	private String surname;
 	@Column(name="status")
-	private boolean isActive;
+	private Boolean isActive;
 	
 	@ManyToOne
 	@JoinColumn(name="id_speciality", referencedColumnName="id")
--- src/main/java/apiRest/hospital/entities/Speciality.java
+++ src/main/java/apiRest/hospital/entities/Speciality.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 public class Speciality {
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
-	private int id;
+	private Integer id;
 	
 	private String description;
 	
